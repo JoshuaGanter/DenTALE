3d17a7be7bb969f1b2d867351c8791163ccd42bd
Joshua Ganter <joshua.michael.ganter@hs-furtwangen.de>
2020-07-30 14:34:45 +0200

    added simple transition effect

---
+++ denTALE/Assets/Script/Artifact.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artifact : MonoBehaviour
+{
+    public bool isCursed = true;
+    public GameScene toScene = GameScene.Archiv;
+}
